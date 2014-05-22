9987af02c89ecbe8563c8ffdc3cc65d82230e16c
karupanerura <karupa@cpan.org>
2014-05-22 19:42:22 +0900

    these docs are not required now.
    TOML.pm is using it now.

--- lib/TOML/Parser.pm
+++ lib/TOML/Parser.pm
@@ -186,19 +186,6 @@ TOML::Parser is a simple toml parser.
 This data structure complies with the tests
 provided at L<https://github.com/mojombo/toml/tree/master/tests>.
 
-=head1 WHY?
-
-In my point of view, it's very difficult to maintain C<TOML::from_toml> because -so far as I understand- there's some issues.
-
-Specifically, for example, C<TOML::from_toml> doesn't interpret correctly in some cases.
-In addition, it reports wrong line number when the error occurs.
-(This is because C<TOML::from_toml> deletes the comments and blank lines before it parses.)
-
-I conclude that C<TOML::from_toml> has an architectural feet,
-and that's why I came to an idea of re-creating another implementation in order to solve the problem.
-
-I believe that this is much easier than taking other solutions.
-
 =head1 METHODS
 
 =over
@@ -257,17 +244,6 @@ If use it, You can replace inflate boolean process.
 Transforms a string containing toml to a perl data structure or vice versa.
 
 =back
-
-=head1 BENCHMARK
-
-benchmark: by `author/benchmark.pl`
-
-    Benchmark: timing 10000 iterations of TOML, TOML::Parser...
-          TOML: 11 wallclock secs (11.11 usr +  0.02 sys = 11.13 CPU) @ 898.47/s (n=10000)
-    TOML::Parser:  7 wallclock secs ( 6.84 usr +  0.01 sys =  6.85 CPU) @ 1459.85/s (n=10000)
-                   Rate         TOML TOML::Parser
-    TOML          898/s           --         -38%
-    TOML::Parser 1460/s          62%           --
 
 =head1 SEE ALSO
 
